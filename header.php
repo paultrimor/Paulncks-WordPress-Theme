5d2fa647477cd82b75a732b3bc023573d6c9aab5
paultrimor <trimor.paul@gmail.com>
2019-10-09 09:20:51 -0400

    Add Google Analytics Code, Image Border styles

--- header.php
+++ header.php
@@ -4,6 +4,17 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?> 
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149668476-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+  		function gtag(){dataLayer.push(arguments);}
+  		gtag('js', new Date());
+
+  		gtag('config', 'UA-149668476-1');
+	</script>
+	
 </head>
 <body>
 	<div id="page" class="site">
