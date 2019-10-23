8ebb5739ff4cb378a8c5e04aca47166cfbb813a3
paultrimor <trimor.paul@gmail.com>
2019-10-23 09:40:44 -0400

    minor branding edits

--- template-parts/site-branding.php
+++ template-parts/site-branding.php
@@ -1,5 +1,27 @@
 <div class="site-branding">
 		<div class="custom-logo"><h1>P.T.</h1></div>	
 		
-		<p class="site-info">Site Info section. Write a short 1 paragraph description of this site. This will appear in all locations. Lorem ipsum dolor sit amet consectetus.</p>	
+		<p class="site-info">
+			<i>Working</i> Portfolio & Blog
+			<center>
+			<br>
+			<br>
+
+			<!-- Easter Egg -->
+			<span style="
+				font-size: 0.1em;
+				font-weight: bold; 
+				color: tomato;
+				-moz-user-select: -moz-none;
+   				-khtml-user-select: none;
+   				-webkit-user-select: none;
+   				-ms-user-select: none;
+   				user-select: none;
+			">good job you found me</span>
+
+
+			<br>
+			<br>
+			<center>
+		</p>	
 </div>
