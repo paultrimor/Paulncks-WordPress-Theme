05f6f8c8bffa79412633a19095e3c0371527be2f
paultrimor <trimor.paul@gmail.com>
2020-03-28 14:40:01 -0400

    Add mobile responsive functionality

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,6 +3,71 @@
  * Description: Miscellaneous functions
 **/ 
 
-// Figure out how to lock screen oreintation to 
-// portrait so you don't have to deal with landscape
-// users/ 
+// Loads once the page starts
+jQuery(window).resize(function() {
+	var window_width = jQuery(window).width();
+
+	if (window_width > 0 && window_width < 768) {
+		/* Mobile Adjustments */ 
+		jQuery('body').addClass('mobile')
+			.removeClass('ipad').removeClass('desktop');
+		
+		showBranding();	
+
+	} else if (window_width >= 768 && window_width < 991) {
+		/* iPad Adjustments */ 
+		jQuery('body').addClass('ipad')
+			.removeClass('mobile').removeClass('desktop');
+		closeMenu();
+
+	} else if ( window_width >= 991) {
+		/* Desktop Adjustments */ 
+		jQuery('body').addClass('desktop')
+			.removeClass('ipad').removeClass('mobile');
+		closeMenu();
+	}
+
+}());
+
+// Hide Branding Section when scroll on Mobile
+var lastScrollTop = 0;
+jQuery('body.mobile #content').scroll(function() {
+	var st = jQuery(this).scrollTop();
+	if (st > lastScrollTop) {
+		console.log('scroll up');
+		hideBranding();
+	} else {
+		console.log('scroll dowm');
+		showBranding();
+	}
+   lastScrollTop = st;
+}); 
+
+function openMenu() {
+	jQuery('#menu-main-container')
+		.removeClass('menu-close')
+		.addClass('menu-open');	
+}
+function closeMenu() {
+	jQuery('#menu-main-container')
+		.removeClass('menu-open')
+		.addClass('menu-close');	
+}
+function toggleMenu() {
+	if (!jQuery('#menu-main-container').hasClass('menu-open')) {
+		openMenu();
+	} else {
+		closeMenu();
+	}		
+}
+
+function hideBranding() {
+	jQuery('.header-area')
+		.removeClass('header-show')
+		.addClass('header-hide');
+}
+function showBranding() {
+	jQuery('.header-area')
+		.removeClass('header-hide')
+		.addClass('header-show');
+}
--- template-parts/site-branding.php
+++ template-parts/site-branding.php
@@ -3,25 +3,5 @@
 		
 		<p class="site-info">
 			<i>Working</i> Portfolio & Blog
-			<center>
-			<br>
-			<br>
-
-			<!-- Easter Egg -->
-			<span style="
-				font-size: 0.1em;
-				font-weight: bold; 
-				color: tomato;
-				-moz-user-select: -moz-none;
-   				-khtml-user-select: none;
-   				-webkit-user-select: none;
-   				-ms-user-select: none;
-   				user-select: none;
-			">good job you found me</span>
-
-
-			<br>
-			<br>
-			<center>
 		</p>	
 </div>
