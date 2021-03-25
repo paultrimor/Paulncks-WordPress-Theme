2def8b3531c6ffb6992772bbb05a59a2cb25cb3a
paultrimor <trimor.paul@gmail.com>
2021-03-24 22:50:56 -0400

    Add shortcode for displaying tags & menu

--- functions.php
+++ functions.php
@@ -19,6 +19,28 @@ function paulncks_scripts() {
 	wp_enqueue_script('functions', get_template_directory_uri().'/scripts/functions.js', array(), '', true);
 }
 
+function paulncks_tags_shortcode($atts) {
+	extract($atts);
+	$items = "<ul class='paulncks-tag-shortcode'>";
+	foreach(get_the_tags($postid) as $tag) {
+		$items .= "<li class='tagname-".$tag->slug."'>".$tag->name."</li>";
+	}
+	$items .= "</ul>";
+	return $items;
+}
+
+function paulncks_menu_shortcode($atts) {
+	extract($atts);
+	$menu = "<div class='paulncks-menu-shortcode'><ul>";
+	foreach(wp_get_nav_menu_items($menuid) as $link) {
+		$menu .= "<li><a href='".$link->url."'>".$link->title."</a></li>";
+	}
+	$menu .= "</ul></div>";
+	return $menu;
+}
+
+add_shortcode('paulncks-tags', 'paulncks_tags_shortcode');
+add_shortcode('paulncks-menu', 'paulncks_menu_shortcode');
 add_action('init', 'register_menus');
 add_action('wp_enqueue_scripts', 'paulncks_scripts'); 
 add_action('after_setup_theme', 'theme_supports'); 
